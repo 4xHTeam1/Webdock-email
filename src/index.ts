9260ac916e252def52d436bf6d66129d8b59c535
Mike Handberg Hovedskov <deengo113@gmail.com>
2023-10-03 22:10:55 +0200

    test email post

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,55 @@
 import { Elysia } from "elysia";
+import { cron } from "@elysiajs/cron";
+import axios from "axios";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const instance = axios.create({
+  baseURL: "https://api.postmarkapp.com/",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "X-Postmark-Server-Token": `${process.env.POSTMARK_TOKEN}`,
+  },
+});
+
+const app = new Elysia()
+  .use(
+    cron({
+      name: "SendEmail",
+      pattern: "* * */24 * * *",
+      run() {
+        console.log("Send Email");
+        instance.post("email", {
+          From: "uclfeedback@webdock.io",
+          To: "mikehovedskov@gmail.com", //Ændre til deres mail
+          Subject: "Test",
+          HtmlBody: '<b>Hello</b> <img src="cid:image.jpg"/>',
+          TextBody: "Hello",
+          TrackOpens: true,
+          TrackLinks: "None",
+          MessageStream: "outbound",
+        });
+      },
+    })
+  )
+  .get("/", async () => {
+    const results = await instance.post("email", {
+      From: "uclfeedback@webdock.io",
+      To: "mikehovedskov@gmail.com", //Ændre til deres mail
+      Subject: "Test",
+      HtmlBody: '<b>Hello</b> <img src="cid:image.jpg"/>',
+      TextBody: "Hello",
+      TrackOpens: true,
+      TrackLinks: "None",
+      MessageStream: "outbound",
+    });
+
+    console.log("Send Email");
+    console.log(results.data);
+
+    return results.data;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
